de6761a0d9ecc2a0c4399acda3170ab3cccb86a3
RiadCure <100498353+riadPCA@users.noreply.github.com>
2022-03-01 01:14:34 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+   
+           double l1, l2, l3;
+           Console.Write("Introduzca el Valor Numerico del 1 Lado:");
+           l1 = Convert.ToDouble(Console.ReadLine());
+           Console.Write("Introduzca el Valor Numerico del 2 Lado:");
+           l2 = Convert.ToDouble(Console.ReadLine());
+           Console.Write("Introduzca el Valor Numerico del 3 Lado:");
+           l3 = Convert.ToDouble(Console.ReadLine());
+           if (l1 == l2 && l2 == l3)
+               Console.WriteLine("\nEl Triangulo es Equilatero");
+           else
+           {
+               if (l1 == l2 || l1 == l3 || l2 == l3)
+                   Console.WriteLine("\nEl Triangulo es Isoceles");
+             }
+  
+}
+}
